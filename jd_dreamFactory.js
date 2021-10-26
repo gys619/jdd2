81a6db59faa3c6acabb2cac2afc17cbf410e7f5f
gys619 <1554537386@qq.com>
2021-10-26 18:30:30 +0800

    更新

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -44,9 +44,6 @@ const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%2
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
  
-];
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-$.tuanIds = [];
 $.appId = 10001;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
